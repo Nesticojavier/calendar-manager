1fee9756f31bb8bdbdcb91ab5582707531b03767
Nestor Javier <Nestico@Nestors-MacBook-Pro.local>
2023-06-04 11:59:57 -0400

    use navLink in instead of href

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -3,11 +3,13 @@ import AddIcon from '@mui/icons-material/Add'
 import SummarizeIcon from '@mui/icons-material/Summarize'
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
 import React, { useState } from 'react'
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   const [selectedIndex, setSelectedIndex] = useState(null)
 
   const handleListItemClick = (event, index) => {
+    // event.preventDefault();
     setSelectedIndex(index)
   }
 
@@ -19,67 +21,76 @@ export default function Sidebar() {
       borderRight={1}
       borderColor="divider"
     >
-      <List sx={{ marginTop: '20px'}}>
-          <ListItem disablePadding>
-            <ListItemButton
-              component="a"
-              href="/WorkCreation"
-              selected={selectedIndex === 0}
-              onClick={(event) =>
-                handleListItemClick(event,0)}
-              sx = {{
-                '&.Mui-selected': { backgroundColor: '#fbc2eb' ,
-                  color: 'black',
-                 '&:hover': {backgroundColor: '#d8b7f7'}
-                }
-              }}
-            >
-              <ListItemIcon>
-                <AddIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Crear trabajo voluntario" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton
-              component="a"
-              href="/WorkList"
-              selected={selectedIndex === 1}
-              onClick={(event) =>
-                handleListItemClick(event,1)}
-              sx = {{
-                '&.Mui-selected': { backgroundColor: '#fbc2eb' ,
-                  color: 'black',
-                  '&:hover': {backgroundColor: '#d8b7f7'}
-                }
-              }}
-            >
-              <ListItemIcon>
-                <SummarizeIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Ver trabajos existentes" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton
-              component="a"
-              href="/dashboard"
-              selected={selectedIndex === 2}
-              onClick={(event) =>
-                handleListItemClick(event,2)}
-              sx = {{
-               '&.Mui-selected': { backgroundColor: '#fbc2eb' ,
-                  color: 'black',
-                  '&:hover': {backgroundColor: '#d8b7f7'}
-                }
-              }}
-            >
-              <ListItemIcon>
-                <CalendarMonthIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Ver calendario" />
-            </ListItemButton>
-          </ListItem>
+      <List sx={{ marginTop: '20px' }}>
+        <ListItem disablePadding>
+          <ListItemButton
+            component={NavLink}
+            // component="a"
+            to="/WorkCreation"
+            // href="/WorkCreation"
+            selected={selectedIndex === 0}
+            onClick={(event) =>
+              handleListItemClick(event, 0)}
+            sx={{
+              '&.Mui-selected': {
+                backgroundColor: '#fbc2eb',
+                color: 'black',
+                '&:hover': { backgroundColor: '#d8b7f7' }
+              }
+            }}
+          >
+            <ListItemIcon>
+              <AddIcon />
+            </ListItemIcon>
+            <ListItemText primary="Crear trabajo voluntario" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton
+            // component="a"
+            // href="/WorkList"
+            component={NavLink}
+            to="/WorkList"
+            selected={selectedIndex === 1}
+            onClick={(event) =>
+              handleListItemClick(event, 1)}
+            sx={{
+              '&.Mui-selected': {
+                backgroundColor: '#fbc2eb',
+                color: 'black',
+                '&:hover': { backgroundColor: '#d8b7f7' }
+              }
+            }}
+          >
+            <ListItemIcon>
+              <SummarizeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Ver trabajos existentes" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton
+            // component="a"
+            // href="/dashboard"
+            component={NavLink}
+            to="/dashboard"
+            selected={selectedIndex === 2}
+            onClick={(event) =>
+              handleListItemClick(event, 2)}
+            sx={{
+              '&.Mui-selected': {
+                backgroundColor: '#fbc2eb',
+                color: 'black',
+                '&:hover': { backgroundColor: '#d8b7f7' }
+              }
+            }}
+          >
+            <ListItemIcon>
+              <CalendarMonthIcon />
+            </ListItemIcon>
+            <ListItemText primary="Ver calendario" />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   )
