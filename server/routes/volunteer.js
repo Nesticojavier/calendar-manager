49b3872a23199c2f59375dfd86c42f9b5d524e6b
Jose Perez <16-10882@usb.ve>
2023-06-17 13:22:43 -0400

    .

--- server/routes/volunteer.js
+++ server/routes/volunteer.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-} = require("../controllers/volunteer.controller");
-
+const {getAllJobs, getOneJob, getJobByMonth } = require("../controllers/volunteer.controller");
 
 // Show all jobs
+router.get("/jobs", getAllJobs);
 
 // Show a job
+router.get("/jobs/:id", getOneJob);
 
-// Postule
+// Show jobs by mouth
+router.get("/jobs/mouth/:month", getJobByMonth );
 
 // Filter
-
-
-
 
 module.exports = router;
