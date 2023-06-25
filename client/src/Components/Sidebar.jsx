42510e9ad5c61a327757c465708a7b2fb64d896e
Nestor Javier <Nestico@MacBook-Pro.local>
2023-06-24 23:50:11 -0400

    sidebar modify : sidebar now show the user profile name

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -9,11 +9,15 @@ import {
 import AddIcon from "@mui/icons-material/Add";
 import SummarizeIcon from "@mui/icons-material/Summarize";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import EventRepeatIcon from '@mui/icons-material/EventRepeat';
+import EventRepeatIcon from "@mui/icons-material/EventRepeat";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../Context/UserContext";
+import { useContext } from "react";
 
 export default function Sidebar() {
+  const { profile } = useContext(UserContext);
   const [selectedIndex, setSelectedIndex] = useState(null);
 
   const handleListItemClick = (event, index) => {
@@ -25,19 +29,45 @@ export default function Sidebar() {
     <Box
       flex={1}
       p={1}
-      sx={{ display: "flex", flexDirection: "column", minHeight: "100vh", minWidth: "30vh" }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        minWidth: "30vh",
+      }}
       borderRight={1}
       borderColor="divider"
     >
       <List sx={{ marginTop: "20px" }}>
+
+        <ListItem disablePadding>
+          <ListItemButton
+            component={NavLink}
+            // to="workcreation"
+            selected={selectedIndex === 0}
+            onClick={(event) => handleListItemClick(event, 0)}
+            sx={{
+              "&.Mui-selected": {
+                backgroundColor: "rgb(180, 190, 247)",
+                color: "black",
+                "&:hover": { backgroundColor: "rgb(127, 145, 248)" },
+              },
+            }}
+          >
+            <ListItemIcon>
+              <AccountCircleIcon sx={{ fontSize: 40 }} />
+            </ListItemIcon>
+            <ListItemText primary={profile?.fullName} />
+          </ListItemButton>
+        </ListItem>
         <ListItem disablePadding>
           <ListItemButton
             component={NavLink}
             // component="a"
             to="workcreation"
             // href="/WorkCreation"
-            selected={selectedIndex === 0}
-            onClick={(event) => handleListItemClick(event, 0)}
+            selected={selectedIndex === 1}
+            onClick={(event) => handleListItemClick(event, 1)}
             sx={{
               "&.Mui-selected": {
                 backgroundColor: "rgb(180, 190, 247)",
@@ -58,8 +88,8 @@ export default function Sidebar() {
             // href="/WorkList"
             component={NavLink}
             to="worklist"
-            selected={selectedIndex === 1}
-            onClick={(event) => handleListItemClick(event, 1)}
+            selected={selectedIndex === 2}
+            onClick={(event) => handleListItemClick(event, 2)}
             sx={{
               "&.Mui-selected": {
                 backgroundColor: "rgb(180, 190, 247)",
@@ -79,8 +109,8 @@ export default function Sidebar() {
           <ListItemButton
             component={NavLink}
             to="workinprogress"
-            selected={selectedIndex === 2}
-            onClick={(event) => handleListItemClick(event, 2)}
+            selected={selectedIndex === 3}
+            onClick={(event) => handleListItemClick(event, 3)}
             sx={{
               "&.Mui-selected": {
                 backgroundColor: "rgb(180, 190, 247)",
@@ -102,8 +132,8 @@ export default function Sidebar() {
             // href="/dashboard"
             component={NavLink}
             to="calendar"
-            selected={selectedIndex === 3}
-            onClick={(event) => handleListItemClick(event, 3)}
+            selected={selectedIndex === 4}
+            onClick={(event) => handleListItemClick(event, 4)}
             sx={{
               "&.Mui-selected": {
                 backgroundColor: "rgb(180, 190, 247)",
--- client/src/Components/SidebarVolunteer.jsx
+++ client/src/Components/SidebarVolunteer.jsx
@@ -9,10 +9,14 @@ import {
 import PersonIcon from "@mui/icons-material/Person";
 import SummarizeIcon from "@mui/icons-material/Summarize";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../Context/UserContext";
+import { useContext } from "react";
 
 export default function SidebarVolunteer() {
+  const { profile } = useContext(UserContext);
   const [selectedIndex, setSelectedIndex] = useState(null);
 
   const handleListItemClick = (event, index) => {
@@ -24,19 +28,46 @@ export default function SidebarVolunteer() {
     <Box
       flex={1}
       p={1}
-      sx={{ display: "flex", flexDirection: "column", minHeight: "100vh", minWidth: "20vh" }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        minWidth: "20vh",
+      }}
       borderRight={1}
       borderColor="divider"
     >
       <List sx={{ marginTop: "20px" }}>
         <ListItem disablePadding>
           <ListItemButton
+            component={NavLink}
+            // component="a"
+            to="myprofile"
+            // href="/WorkCreation"
+            selected={selectedIndex === 0}
+            onClick={(event) => handleListItemClick(event, 0)}
+            sx={{
+              "&.Mui-selected": {
+                backgroundColor: "rgb(180, 190, 247)",
+                color: "black",
+                "&:hover": { backgroundColor: "rgb(127, 145, 248)" },
+              },
+            }}
+          >
+            <ListItemIcon>
+              <AccountCircleIcon sx={{ fontSize: 40 }} />
+            </ListItemIcon>
+            <ListItemText primary={profile?.fullName} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton
             // component="a"
             // href="/dashboard"
             component={NavLink}
             to="calendar"
-            selected={selectedIndex === 0}
-            onClick={(event) => handleListItemClick(event, 0)}
+            selected={selectedIndex === 1}
+            onClick={(event) => handleListItemClick(event, 1)}
             sx={{
               "&.Mui-selected": {
                 backgroundColor: "rgb(180, 190, 247)",
@@ -49,28 +80,6 @@ export default function SidebarVolunteer() {
               <CalendarMonthIcon />
             </ListItemIcon>
             <ListItemText primary="Ver calendario" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton
-            component={NavLink}
-            // component="a"
-            to="myprofile"
-            // href="/WorkCreation"
-            selected={selectedIndex === 1}
-            onClick={(event) => handleListItemClick(event, 1)}
-            sx={{
-              "&.Mui-selected": {
-                backgroundColor: "rgb(180, 190, 247)",
-                color: "black",
-                "&:hover": { backgroundColor: "rgb(127, 145, 248)" },
-              },
-            }}
-          >
-            <ListItemIcon>
-              <PersonIcon />
-            </ListItemIcon>
-            <ListItemText primary="Perfil" />
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
