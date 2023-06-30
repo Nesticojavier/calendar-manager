2f474216fb891c9696ca7c596ff35081ced19e6e
TU_NOMBRE <TU_CORREO>
2023-06-30 12:57:24 -0400

    now the volunteer can to see his works in status 'por confirmar'

--- client/src/Components/ProviderComponents/WorkListInProgress.jsx
+++ client/src/Components/ProviderComponents/WorkListInProgress.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Pagination } from "@mui/material";
+import { Box } from "@mui/material";
 import WorkStatusNavbar from "../WorkStatusNavbar";
 import ConfirmedListWorkProvider from "./ConfirmedListWorkProvider";
 
@@ -7,23 +7,10 @@ export default function WorkListInProgress() {
   // state for status verify work
   const [statusConfirmed, setStatusConfirmed] = useState(true);
 
-  //state for set current page in the pagination
-  const [currentPage, setCurrentPage] = useState(1);
-
   // handle for change status for to show
   const handleChangeStatus = (value) => {
     setStatusConfirmed(value);
   };
-
-  // handle for change page number page value
-  const handlePageChange = (event, page) => {
-    setCurrentPage(page);
-  };
-
-  // When change status view, the page number should be reset to 1
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [statusConfirmed]);
 
   return (
     <Box
@@ -41,20 +28,7 @@ export default function WorkListInProgress() {
         handleChangeStatus={handleChangeStatus}
       />
 
-      <ConfirmedListWorkProvider
-        currentPage={currentPage}
-        statusConfirmed={statusConfirmed}
-      />
-
-      <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
-        <Pagination
-          onChange={handlePageChange}
-          page={currentPage}
-          count={10}
-          variant="outlined"
-          color="primary"
-        />
-      </Box>
+      <ConfirmedListWorkProvider statusConfirmed={statusConfirmed} />
     </Box>
   );
 }
