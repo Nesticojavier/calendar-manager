7996e25b1896971354aa0a7312b86ce79c50cdc6
TU_NOMBRE <TU_CORREO>
2023-07-14 18:46:36 -0400

    pdf services form the generating these

--- server/controllers/reports.controller.js
+++ server/controllers/reports.controller.js
@@ -1,53 +1,24 @@
 const reportsService = require("../services/reports.service");
 const serverErrors = require("../error/error");
-const PDFDocument = require("pdfkit-table");
 const fs = require("fs");
 const path = require("path");
+const pdfService = require("../services/file-generation/pdf.service");
 
 const getReportProvider = async (req, res) => {
   const outputPath = path.join(__dirname, "files", "output.pdf");
-  const { id: users_id } = req.userData;
+  const { id: users_id, username } = req.userData;
 
   try {
-    //get data
+    //get data using database's service
     const data = await reportsService.getReportProvider(users_id);
 
-    // construct pdf
-    const doc = new PDFDocument({ margin: 30, size: "A4" });
-
-    // set path to save pdf
-    // doc.pipe(fs.createWriteStream(outputPath));
-    
-    // function to generate pdf
-    await generatePDFTable(doc, data);
-    doc.pipe(res)
-    
-    doc.end();
+    // use pdf generation services
+    await pdfService.generatePDFTable(data, res, username);
   } catch (error) {
     return res
       .status(error?.status || 500)
       .json({ status: "FAILED", data: { error: error?.message || error } });
   }
-
-  // return res.json({ message: "exitoso" });
-};
-
-const generatePDFTable = async (doc, data) => {
-  const table = {
-    title: "Mis trabajos",
-    headers: ["titulo", "descripcion", "fecha de inicio", "fecha de fin"],
-    rows: data.map((work) => [work.title, work.description, work.dateInit, work.dateEnd]),
-  };
-
-  const startX = 50;
-  const startY = 50;
-  const tableOptions = {
-    x: startX,
-    y: startY,
-    width: 500,
-  };
-
-  doc.table(table, tableOptions);
 };
 
 module.exports = {
---
+++ server/services/file-generation/pdf.service.js
@@ -0,0 +1,190 @@
+const PDFDocument = require("pdfkit-table");
+
+const generatePDFTable = async (data, res, user) => {
+  // construct pdf
+  const doc = new PDFDocument({ margin: 30, size: "A4" });
+
+  // construct table
+  const table = {
+    title: `Trabajos de @${user}`,
+    headers: ["titulo", "descripcion", "fecha de inicio", "fecha de fin"],
+    rows: data.map((work) => [
+      work.title,
+      work.description,
+      work.dateInit,
+      work.dateEnd,
+    ]),
+  };
+
+  doc.table(table);
+
+  doc.pipe(res);
+
+  doc.end();
+};
+
+/**
+ * Generates a report in PDF format with a provider trace and sends the response
+ * via the response object (res).
+ *
+ * @param {Array} data - provider tracking data
+ *             Each element of the array must be an object with the following keys:
+ *              - title
+ *              - type
+ *              - username
+ *              - fullName
+ *              - institutionalId
+ *              - date
+ *              - hour
+ *              - attendance
+ * @param {Object} res - The HTTP response object used to send the response.
+ * @param {string} providerUsername - provider's username.
+ * @returns {Promise<void>} - A promise that resolves when the report has
+ *                            been generated and the response sent.
+ */
+const providerTrackingReport = async (data, res, providerUsername) => {
+  // construct pdf
+  const doc = new PDFDocument({ margin: 30, size: "A4" });
+
+  // construct table
+  const table = {
+    title: `Seguimiento del proveedor @${providerUsername}`,
+    headers: [
+      "Trabajo",
+      "Tipo de trabajo",
+      "Nombre de usuario",
+      "Nombre completo",
+      "Id institucional",
+      "Fecha",
+      "Hora",
+      "Asistencia",
+    ],
+    rows: data.map((item) => [
+      item.title,
+      item.type,
+      item.username,
+      item.fullName,
+      item.institutionalId,
+      item.date,
+      item.hour,
+      item.attendance,
+    ]),
+  };
+
+  doc.table(table);
+
+  doc.pipe(res);
+
+  doc.end();
+};
+
+/**
+ * Generate a report in PDF format with the provider's applications and send
+ * the response via the response object (res).
+ *
+ * @param {Array} data - provider postulations data
+ *             Each element of the array must be an object with the following keys:
+ *              - title
+ *              - type
+ *              - username
+ *              - fullName
+ *              - institutionalId
+ *              - dateCreatedPostulation
+ *              - statePostulation
+ * @param {Object} res - The HTTP response object used to send the response.
+ * @param {string} providerUsername - provider's username.
+ * @returns {Promise<void>} - A promise that resolves when the report has
+ *                            been generated and the response sent.
+ */
+const providerPostulationsReport = async (data, res, providerUsername) => {
+  // construct pdf
+  const doc = new PDFDocument({ margin: 30, size: "A4" });
+
+  // construct table
+  const table = {
+    title: `Postulaciones del proveedor @${providerUsername}`,
+    headers: [
+      "Trabajo",
+      "Tipo de trabajo",
+      "Nombre de usuario",
+      "Nombre completo",
+      "Id institucional",
+      "Fecha creación postulación",
+      "Estado de la postulación",
+    ],
+    rows: data.map((item) => [
+      item.title,
+      item.type,
+      item.username,
+      item.fullName,
+      item.institutionalId,
+      item.dateCreatedPostulation,
+      item.statePostulation,
+    ]),
+  };
+
+  doc.table(table);
+
+  doc.pipe(res);
+
+  doc.end();
+};
+
+/**
+ * Generates a report in PDF format with a volunteer trace and sends the response
+ * via the response object (res).
+ *
+ * @param {Array} data - provider postulations data
+ *             Each element of the array must be an object with the following keys:
+ *              - title
+ *              - type
+ *              - username
+ *              - fullName
+ *              - date
+ *              - hour
+ *              - attendance
+ * @param {Object} res - The HTTP response object used to send the response.
+ * @param {string} volunteerUsername - volunteer's username.
+ * @returns {Promise<void>} - A promise that resolves when the report has
+ *                            been generated and the response sent.
+ */
+const volunteerTrackingReport = async (data, res, volunteerUsername) => {
+  // construct pdf
+  const doc = new PDFDocument({ margin: 30, size: "A4" });
+
+  // construct table
+  const table = {
+    title: `Seguimiento del voluntario @${volunteerUsername}`,
+    headers: [
+      "Trabajo",
+      "Tipo de trabajo",
+      "Nombre de usuario",
+      "Nombre completo",
+      "Fecha",
+      "Hora",
+      "Asistencia",
+    ],
+    rows: data.map((item) => [
+      item.title,
+      item.type,
+      item.username,
+      item.fullName,
+      item.date,
+      item.hour,
+      item.attendance,
+    ]),
+  };
+
+  doc.table(table);
+
+  doc.pipe(res);
+
+  doc.end();
+};
+
+module.exports = {
+  generatePDFTable,
+  providerPostulationsReport,
+  providerTrackingReport,
+  volunteerTrackingReport,
+};
